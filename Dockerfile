cf4e1c1ad19e3bea3613d2cfb9944240d2107836
mothcrown <uchodc@gmail.com>
2025-03-26 15:51:46 +0000

    More fixes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM nvidia/cuda:12.6.2-runtime-ubuntu22.04
+FROM nvidia/cuda:12.6.1-runtime-ubuntu22.04
 
 COPY ./init.sh init.sh
 RUN chmod +x init.sh
 
-RUN apt-get update && apt-get install -y wget python3 python3-pip python3-venv git nvidia-container-toolkit
+RUN apt-get update && apt-get install -y wget python3.10 python3-pip python3.10-venv git nvidia-cuda-toolkit nvidia-driver-550 nvidia-container-toolkit
 
-ENTRYPOINT ["/init.sh"]
+ENTRYPOINT ["sh", "/init.sh"]
 
 EXPOSE 8188
--- init.sh
+++ init.sh
@@ -6,23 +6,20 @@ cd /env
 if [ ! -d "comfy" ]; then
     python3 -m venv comfy
 fi
-source comfy/bin/activate
+. ./comfy/bin/activate
 
 # Clone ComfyUI if not already cloned
-if [ ! -d "/env/ComfyUI" ]; then
-    git clone https://github.com/comfyanonymous/ComfyUI.git /env/ComfyUI
+if [ ! -d "ComfyUI" ]; then
+    git clone https://github.com/comfyanonymous/ComfyUI.git ComfyUI
 fi
 
 # Download model and lora
-mkdir -p /env/ComfyUI/models/checkpoints
-mkdir -p /env/ComfyUI/models/loras
-
-if [ ! -f "/env/ComfyUI/models/checkpoints/sd_xl_base_1.0.safetensors" ]; then
-    wget -P /env/ComfyUI/models/checkpoints https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+if [ ! -f "ComfyUI/models/checkpoints/sd_xl_base_1.0.safetensors" ]; then
+    wget -P ComfyUI/models/checkpoints https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
 fi
 
-if [ ! -f "/env/ComfyUI/models/loras/PS1Redmond-PS1Game-Playstation1Graphics.safetensors" ]; then
-    wget -P /env/ComfyUI/models/loras https://huggingface.co/artificialguybr/ps1redmond-ps1-game-graphics-lora-for-sdxl/resolve/main/PS1Redmond-PS1Game-Playstation1Graphics.safetensors
+if [ ! -f "ComfyUI/models/loras/PS1Redmond-PS1Game-Playstation1Graphics.safetensors" ]; then
+    wget -P ComfyUI/models/loras https://huggingface.co/artificialguybr/ps1redmond-ps1-game-graphics-lora-for-sdxl/resolve/main/PS1Redmond-PS1Game-Playstation1Graphics.safetensors
 fi
 
 # Install dependencies
